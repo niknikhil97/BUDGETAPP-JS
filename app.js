8ac8060394918c84244fbff95430e71d72e2d6e0
Nikhil <nik.nikhil97@gmail.com>
2020-07-10 16:50:02 +0530

    Budget data structures added

--- app.js
+++ app.js
@@ -5,6 +5,27 @@
 // BUDGET CONTROLLER
 var budgetController = (function () {
   // some code
+  var Expenses = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: [],
+    },
+    totals: {
+      exp: 0,
+      inc: 0,
+    },
+  };
 })();
 
 // UI CONTROLLER

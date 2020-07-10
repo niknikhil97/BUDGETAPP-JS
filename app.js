6c5ed57d5c5ec00105e3fa5477bdda8363c25eae
Nikhil <nik.nikhil97@gmail.com>
2020-07-10 15:53:29 +0530

    Till taking inputs.

---
+++ app.js
@@ -0,0 +1,54 @@
+/*************************************************
+ * MAIN PROGRAM
+ */
+
+// BUDGET CONTROLLER
+var budgetController = (function () {
+  // some code
+})();
+
+// UI CONTROLLER
+var UIController = (function () {
+  // some code
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputButton: ".add__btn",
+  };
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
+})();
+
+// GLOBAL APP CONTROLLER
+var Controller = (function (budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMstrings();
+  var ctrlAddItem = function () {
+    // 1. select the add field
+    var input = UIController.getInput();
+    console.log(input);
+    // 2. add item to budget
+    // 3. add the item to the UI
+    // 4. calculate the budget
+    // 5. display the budget on the ui
+  };
+  document
+    .querySelector(DOM.inputButton)
+    .addEventListener("click", ctrlAddItem);
+
+  document.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
+})(budgetController, UIController);
